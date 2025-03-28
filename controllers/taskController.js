ca894979941fba74aebabd69fd3bdefd370a19a8
ram pandey <ramkrishan.pandey@dotsquares.com>
2025-03-28 18:58:47 +0530

    v-0.0.4 Adding validations

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -114,6 +114,21 @@ exports.createTask = function (req, res) {
 exports.updateTask = function (req, res) {
     const { title, description, due_date, status } = req.body;
 
+    // Validate required fields with joi
+    const schema = Joi.object({
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        due_date: Joi.date().required(),
+        status: Joi.string().valid('pending', 'in-progress', 'completed').required(),
+    });
+    const { error } = schema.validate(req.body);
+    if (error) {
+        return res.status(400).json({
+            status: 400,
+            success: false, message: error.details[0].message
+        });
+    }
+
     // validate id
     if (!req.params.id) {
         return res.status(400).json({
